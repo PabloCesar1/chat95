6bdcaf00192a17ca62b7f68077fe09cfd47f8109
pablo_cesar1995@hotmail.com <pablo_cesar1995@hotmail.com>
2017-11-02 19:50:38 -0500

    isAuthenticated

--- models/chat.js
+++ models/chat.js
@@ -3,6 +3,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var ChatSchema = Schema({
 	name: String,
-	message: String
+	message: String,
+	createdAt: { type: Date, default: Date.now },
 });
 module.exports = mongoose.model('Chat', ChatSchema);
--- routes/chat.js
+++ routes/chat.js
@@ -5,8 +5,17 @@ var chatController = require('../controllers/chat');
 var loginController = require('../controllers/login');
 var api = express.Router();
 
+
+function isAuthenticated(req, res, next) {
+    if (req.user) {
+        next();
+    } else {
+        res.redirect('/login');
+    }
+}
+
 //api.get('/index', chatController.getView);
-api.get('/', chatController.getView);
+api.get('/', isAuthenticated, chatController.getView);
 //api.get('/auth/facebook', loginController.loginFacebook);
 //api.get('/auth/facebook/callback', loginController.callbackFacebook);
 //api.get('/logout', loginController.logout);
