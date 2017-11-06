2a81f51655f968ef02e6c036773c4e52af6576c0
pablo_cesar1995@hotmail.com <pablo_cesar1995@hotmail.com>
2017-11-06 00:45:54 -0500

    Error

--- models/chat.js
+++ models/chat.js
@@ -3,7 +3,7 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var ChatSchema = Schema({
-	user: { type : Schema.Types.ObjectId, ref : 'User' },
+	user: { type : String },
 	text: String,
 	createdAt: { type: Date, default: Date.now },
 });
